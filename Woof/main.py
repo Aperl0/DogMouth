197c63dc781f51333d5d90e6e7ccf8112d7f992e
palamagoo <abrahamandeggslincoln@gmail.com>
2023-02-09 11:55:01 -0800

    add intro message

--- Woof/main.py
+++ Woof/main.py
@@ -1,6 +1,40 @@
+import os
+import time
+
 #!/usr/bin/env python3
 
 
 def main():
-    print('Hello World')
-    print('Give Mr. Granger admin privilleges')
+    print("PALAMAGOO DATING SIM")
+    print("===--------------===")
+
+    print("===========")
+    print("=  o   o  =")
+    print("=    |    =")
+    print("=         =")
+    print("=  \___/  =")
+    print("===========")
+    print()
+    print("Welcome to the game")
+    print("Enter your name down below.")
+    name = str(input("> "))
+    print()
+    time.sleep(2)
+    os.system('clear')
+    if name.lower() == "palamagoo":
+        print("===\===/===")
+        print("=  o   o  =")
+        print("=    |    =")
+        print("=   ___   =")
+        print("=  /   \  =")
+        print("===========")
+        print()
+        print("Thats... MY NAME!!!! YOU LITTLE PIECE OF SHIT")
+        print("You were brutally stabbed 28.3 times")
+    else:
+        print("So, you're name is " + name + "? What a loveeeeely name. My name Palamagoo!!")
+        print("So what do you wanna do today my lovely little darling lovely?")
+    
+
+
+if __name__ == '__main__': main()
